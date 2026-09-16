1e346b8f1367ca530dd05519e7ed85e3e19b7bd0
Anna Onyshchenko <auonischenko@gmail.com>
2026-09-16 18:10:43 +0300

    Final fix for clean API endpoints configuration

--- client/src/services/constants.ts
+++ client/src/services/constants.ts
@@ -1,4 +1,7 @@
+
 export const API_URL = 'https://find-yourself-backend.onrender.com/api';
+
+export const API_LOGIN_URL = `${API_URL}/auth/login`;
 
 export const API_REGISTER_URL = `${API_URL}/register`;
 
